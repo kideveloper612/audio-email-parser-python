ae0185820055514ca37de5c0fc6a1afeed927438
kideveloper612 <51858897+kideveloper612@users.noreply.github.com>
2020-12-05 17:58:25 +0800

    complete

--- mail.py
+++ mail.py
@@ -1,6 +1,7 @@
 import imaplib
 import email
 import re
+import glob
 import dotenv
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -8,41 +9,21 @@ from google.auth.transport.requests import Request
 import os
 import pickle
 import time
+from dateutil.parser import parse
+from googleapiclient.http import MediaFileUpload
 
 dotenv.load_dotenv()
 username = os.environ.get('EMAIL')
 password = os.environ.get('PASSWORD')
 source = os.environ.get('FROM')
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
 
 SAMPLE_SPREADSHEET_ID_input = '1aTeq8ndBQxBpvgyx8GES4tnW_Di_iFyXTkBpinDTCgk'
 SAMPLE_RANGE_NAME = 'A1:AA1000000'
 
 
-def get_rows_from_sheet():
-    creeds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creeds = pickle.load(token)
-    if not creeds or not creeds.valid:
-        if creeds and creeds.expired and creeds.refresh_token:
-            creeds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creeds = flow.run_local_server(port=0)
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creeds, token)
-
-    service = build('sheets', 'v4', credentials=creeds)
-
-    sheet = service.spreadsheets()
-    result_input = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID_input, range=SAMPLE_RANGE_NAME).execute()
-    values_input = result_input.get('values', [])
-    return len(values_input)
-
-
-def write_sheet(records):
+def write_sheet(records, filenames):
     creeds = None
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
@@ -62,10 +43,9 @@ def write_sheet(records):
     result_input = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID_input, range=SAMPLE_RANGE_NAME).execute()
     values_input = result_input.get('values', [])
 
-    column = ['Date', 'Name', 'Phone Number', 'Voicemail Duration', 'Transcribed Voicemail', 'Misc.']
+    column = ['Date', 'Name', 'Phone Number', 'Voicemail Duration', 'Transcribed Voicemail', 'Misc.', 'File']
     if len(values_input) == 0:
         records.insert(0, column)
-    print(len(values_input))
 
     sheet.values().append(
         spreadsheetId=SAMPLE_SPREADSHEET_ID_input,
@@ -76,56 +56,74 @@ def write_sheet(records):
             values=records)
     ).execute()
 
+    serviceDrive = build('drive', 'v3', credentials=creeds)
+
+    results = serviceDrive.files().list(
+        pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    items = [item['name'] for item in results.get('files', [])]
+
+    for filename in filenames:
+        if filename in items:
+            continue
+        file_metadata = {'name': filename}
+        media = MediaFileUpload('{}'.format(filename), mimetype='audio/mpeg')
+        serviceDrive.files().create(body=file_metadata, media_body=media, fields='id').execute()
+
+
+def getPayload(msg):
+    if msg.is_multipart():
+        return getPayload(msg.get_payload(0))
+    return msg.get_payload(None, True)
+
 
 def main():
     global source
-
-    if not os.path.isdir(fileDirectory):
-        os.mkdir(fileDirectory)
     while True:
         print('============ START NEW LOOP =============')
+        for file in glob.glob("./*.mp3"):
+            os.remove(file)
         imap = imaplib.IMAP4_SSL("imap.gmail.com", 993)
         imap.login(username, password)
         imap.select("INBOX")
         sub_status, messages = imap.search(None, '(SEEN FROM {})'.format(source))
         lines = []
+        filenames = []
         if sub_status == 'OK':
-            records_count = get_rows_from_sheet()
-            print(type(records_count), records_count)
             for message in messages[0].split():
                 res, msg = imap.fetch(message, "(RFC822)")
                 for response in msg:
                     try:
+                        fileName = ''
                         if isinstance(response, tuple):
                             msg = email.message_from_bytes(response[1])
-                            for part in msg.walk():
-                                payload = part.get_payload(decode=True)
-                                if payload is not None:
-                                    text = payload.decode('utf-8').replace('\r', '').replace('\n', ' ')
-                                    date = re.search('Time:(.*)From:', text).group(1).strip()
-                                    namePhone = re.search('Time:(.*)Duration:', text).group(1).replace('From:', '').replace(date, '').strip()
-                                    name = namePhone.split('(')[0].strip()
-                                    phone = namePhone.replace(name, '').strip()
-                                    duration = re.search('Duration:(.*)Transcript:', text).group(1).strip()
-                                    transcript = re.search('Transcript:(.*)Voicemail box:', text).group(1).split('Rate this transcript')[0].strip()
-                                    misc = re.search('Voicemail box:(.*)Sincerely', text).group(1).strip()
-                                    line = [date, name, phone, duration, transcript, misc]
-                                    print(line)
-                                    records_count += 1
-                                    lines.append(line)
-                                    break
+                            payload = getPayload(msg)
+                            if payload is not None:
+                                text = payload.decode('utf-8').replace('\r', '').replace('\n', ' ')
+                                date = re.search('Time:(.*)From:', text).group(1).strip()
+                                namePhone = re.search('Time:(.*)Duration:', text).group(1).replace('From:', '').replace(date, '').strip()
+                                name = namePhone.split('(')[0].strip()
+                                phone = namePhone.replace(name, '').strip()
+                                duration = re.search('Duration:(.*)Transcript:', text).group(1).strip()
+                                transcript = re.search('Transcript:(.*)Voicemail box:', text).group(1).split('Rate this transcript')[0].strip()
+                                misc = re.search('Voicemail box:(.*)Sincerely', text).group(1).strip()
+                                fileDate = parse(date).date()
+                                fileTime = str(parse(date).time()).replace(':', '~')
+                                fileName = '{}_{}_{}.mp3'.format(name, fileDate, fileTime)
+                                line = [date, name, phone, duration, transcript, misc, fileName]
+                                print(line)
+                                lines.append(line)
                             for part in msg.walk():
                                 if part.get_content_maintype() == 'multipart':
                                     continue
                                 if part.get('Content-Disposition') is None:
                                     continue
-                                fileName = part.get_filename()
-                                if bool(fileName):
-                                    filePath = os.path.join(fileDirectory, '{}.mp3'.format(records_count))
-                                    if not os.path.isfile(filePath):
-                                        fp = open(filePath, 'wb')
+                                fileFlag = part.get_filename()
+                                if bool(fileFlag):
+                                    if not os.path.isfile(fileName):
+                                        fp = open(fileName, 'wb')
                                         fp.write(part.get_payload(decode=True))
                                         fp.close()
+                                    filenames.append(fileName)
                     except Exception as e:
                         print(e)
                         continue
@@ -135,12 +133,11 @@ def main():
         imap.logout()
 
         if len(lines) > 0:
-            write_sheet(records=lines)
+            write_sheet(records=lines, filenames=filenames)
 
         time.sleep(4)
 
 
 if __name__ == '__main__':
-    fileDirectory = 'files'
     main()
 
